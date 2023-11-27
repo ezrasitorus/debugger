ae56db1e1bf9463cfd25a8681af017f6ffea6971
Ezra Sitorus <Ezra.Sitorus@arm.com>
2023-11-27 22:18:12 +0000

    Child/parent fork

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{char::ToLowercase, io};
+use nix::{
+    sys::wait::waitpid,
+    unistd::{fork, write, ForkResult},
+};
+use std::{env, io, process::Command, str::from_utf8};
 
 enum Commands {
     Info { option: String },
@@ -11,6 +15,36 @@ fn main() {
     let mut option: String = String::new();
     print_start();
     print_command_info();
+
+    let cmd_args: Vec<String> = env::args().collect();
+
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!(
+                "Continuing execution in parent process, new child has pid: {}",
+                child
+            );
+            waitpid(child, None).unwrap();
+            println!("Child has finished executing!");
+            std::process::exit(0);
+        }
+        Ok(ForkResult::Child) => {
+            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
+            write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+            unsafe { libc::_exit(0) };
+        }
+        Err(_) => println!("Fork failed"),
+    }
+
+    let echo_child = Command::new(cmd_args[1].as_str())
+        .arg("Hello World")
+        .output()
+        .expect("Failed to start echo process");
+
+    println!(
+        "{:?}",
+        from_utf8(&echo_child.stdout).expect("Failed to read")
+    );
 
     loop {
         option.clear();
